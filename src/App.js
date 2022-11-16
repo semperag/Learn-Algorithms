4c0d9f0caa3da165f4f3402c33b8303c9e343a67
AdiSemper <semperag@dukes.jmu.edu>
2022-11-15 23:39:20 -0500

    Sorting algorithms

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      { //QuickSort
+        //MergeSort
+        //BubbleSort
+        //SelectionSort
+        //HeapSort
+        //InsertionSort
+      }
     </div>
   );
 }
