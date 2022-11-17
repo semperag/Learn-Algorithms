e0022b567ee32dd10ea6f14634f9dc2633113792
AdiSemper <semperag@dukes.jmu.edu>
2022-11-16 21:30:44 -0500

    bubblesort link

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 import React from 'react';
 
 const Navbar = () => {
     return (
         <div className='navbar'>
-
+            <Link to="/">Insertion Sort</Link>
+            <Link to="/bubble-sort">Bubble Sort</Link>
         </div>
     );
 }
