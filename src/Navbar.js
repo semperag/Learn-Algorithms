44774f44e9c2e60e2ec5dd3638ba7c4a13e6d41c
AdiSemper <semperag@dukes.jmu.edu>
2022-11-17 23:42:10 -0500

    changing items length

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,9 +2,22 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 import React from 'react';
 
-const Navbar = () => {
+const Navbar = ({items, setItems}) => {
+    const minLength = 5;
+
+    const changeList = event => {
+        const length = event.target.value;
+        if (length >= minLength) {
+            console.log(length);
+            var newArray = items.slice();    
+            newArray.push(length);   
+            setItems({arr:newArray})
+        }
+    }
+
     return (
         <div className='navbar'>
+            <input onChange={changeList}></input>
             <Link to="/">Insertion Sort</Link>
             <Link to="/bubble-sort">Bubble Sort</Link>
             <Link to="/selection-sort">Selection Sort</Link>
