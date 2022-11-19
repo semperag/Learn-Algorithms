6dc94fcb97ecf3d2eeceaacf8a0964cec3d81362
AdiSemper <semperag@dukes.jmu.edu>
2022-11-18 23:06:54 -0500

    Shuffle array

--- src/App.js
+++ src/App.js
@@ -12,11 +12,29 @@ function App() {
   const [items, setItems] = useState([5,4,2,3,1]);
   let list = Object.assign([], items);
 
+  function shuffleArray(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
   return (
     <div className="App">
       <div className='big'></div>
       <Router>
-        <Navbar items={items} setItems={setItems}/>
+        <Navbar items={items} setItems={setItems} shuffleArray={shuffleArray}/>
           <Routes>
             <Route path="/" element={<ListItems items={items}/>} />
           </Routes>
