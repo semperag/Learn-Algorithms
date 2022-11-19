d431ea991a6b27ad2cb9c93230a8a3c170ae20aa
AdiSemper <semperag@dukes.jmu.edu>
2022-11-18 22:54:30 -0500

    change array's length

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,29 @@
 import { Link } from 'react-router-dom';
+
 import './Navbar.css';
 import React, { useState, useEffect } from 'react';
 
-const Navbar = ({items, setItems}) => {
+const Navbar = ({items, setItems, shuffleArray}) => {
     const minLength = 5;
     let list = Object.assign([], items);
 
     const changeList = event => {
         const length = event.target.value;
-        if (length >= minLength) {
-            list.push(length);
-            setItems(list);
-            console.log(list);
-            console.log("items: " + items);
+        let changingLength = Number(items.length);
+        if (length > 5) {
+            if (Number(length) > Number(changingLength)) {
+                while ( Number(length) > Number(changingLength)) {
+                    list.push(changingLength+1);
+                    changingLength = changingLength + 1;
+                    console.log('here')
+                }
+                setItems(shuffleArray(list));
+                console.log('here')
+            }
+            else if (Number(length) < Number(changingLength)) {
+                const list = Array.from({length: length}, (_, i) => i + 1);
+                setItems(shuffleArray(list));
+            }
         }
     }
 
