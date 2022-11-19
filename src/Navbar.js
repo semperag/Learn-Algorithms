3cd692f1efe27ac8e828354d2cb3e8b3c83e71ae
AdiSemper <semperag@dukes.jmu.edu>
2022-11-19 00:35:08 -0500

    changing animation speed

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,14 +3,16 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 import React, { useState, useEffect } from 'react';
 
-const Navbar = ({items, setItems, shuffleArray}) => {
-    const minLength = 5;
+const minLength = 5;
+const minSpeed = 50;
+
+const Navbar = ({items, setItems, setSpeed, shuffleArray}) => {
     let list = Object.assign([], items);
 
     const changeList = event => {
         const length = event.target.value;
         let changingLength = Number(items.length);
-        if (length > 5) {
+        if (length > minLength) {
             if (Number(length) > Number(changingLength)) {
                 while ( Number(length) > Number(changingLength)) {
                     list.push(changingLength+1);
@@ -27,12 +29,21 @@ const Navbar = ({items, setItems, shuffleArray}) => {
         }
     }
 
+    const changeSpeed = event => {
+        const speed = event.target.value;
+
+        if (speed >= 0 && speed <= 1500) {
+            setSpeed(speed);
+        }
+    }
+
     return (
         <div className='navbar'>
             <input onChange={changeList}></input>
             <Link to="/">Insertion Sort</Link>
             <Link to="/bubble-sort">Bubble Sort</Link>
             <Link to="/selection-sort">Selection Sort</Link>
+            <input onChange={changeSpeed}></input>
         </div>
     );
 }
