da8d10047c930412a43a36afd5b71f9c12c7eabc
AdiSemper <semperag@dukes.jmu.edu>
2022-12-20 22:25:12 -0500

    com

--- src/BubbleSort.js
+++ src/BubbleSort.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './ListItems.css';/
+import './ListItems.css';
 
 const BubbleSort = ({items, speed, setItems}) => {
     const [list, setList] = useState(Object.assign([], items));
