755228debb61d99e82b857207d3991e48f6a1ab2
Adi Sempertegui <semperag@dukes.jmu.edu>
2023-02-22 22:10:42 -0500

    Update InsertionSort.js

--- src/InsertionSort.js
+++ src/InsertionSort.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './ListItems.css';
-
-const BubbleSort = ({items, speed, setItems, sorting, setSorting}) => {
+const InsertionSort = ({items, speed, setItems, sorting, setSorting}) => {
     const [list, setList] = useState(Object.assign([], items));
-    const [length, setLength] = useState(items.length);
+    const [length, setLength] = useState(0);
     const [clicked, setClicked] = useState(false);
     const [currIndex, setCurrIndex] = useState(0);
-    const [upperIndex, setUpperIndex] = useState(currIndex);
+    const [lowerIndex, setLowerIndex] = useState(currIndex-1);
     const [index, setIndex] = useState(0);
     const [end, setEnd] = useState(false);
     const [sorted, setSorted] = useState(false);
@@ -16,40 +15,40 @@ const BubbleSort = ({items, speed, setItems, sorting, setSorting}) => {
     useEffect(() => {
         const handleChange = async () => {
             setTimeout(() => {
-                if (end) {
-                    setIndex(0);
-                    setLength(length-1);
+                if (end || index < 1) {
+                    setIndex(length+1);
+                    setLength(length+1);
                     setEnd(false);
                 }
                 else {
-                    setIndex(index+1);
+                    setIndex(index-1);
                 }
             }, speed);
         };
-
-        if (clicked && length > 0) {
+        if (clicked && length <= items.length) {
             handleChange();
         }
     }, ([list]));
-
     // simulate set interval behavior 
     // each 5 s this function will be re-invoked
     useEffect(() => {
-
-    // wait 5 s before cause a re-render
-    if (clicked && length > 0) {
-        setTimeout(() => {
-        }, speed);
-    }
-    else {
-        setClicked(false);
-        setCurrIndex(0);
-        setUpperIndex(currIndex);
-        setIndex(0);
-        setEnd(false);
-        setSortClicked(false);
-        setSorting(false);
-    }
+        // wait 5 s before cause a re-render
+        if (clicked && length <= items.length) {
+            setTimeout(() => {
+            }, speed);
+        }
+        else {
+            setClicked(false);
+            setSorted(true);
+            setLength(0);
+            setClicked(false);
+            setCurrIndex(0);
+            setLowerIndex(currIndex-1);
+            setIndex(0);
+            setEnd(false);
+            setSortClicked(false);
+            setSorting(false);
+        }
     }, [length]);
 
     useEffect(() => {
@@ -61,35 +60,33 @@ const BubbleSort = ({items, speed, setItems, sorting, setSorting}) => {
         }, [index]);
 
     function sort() {
-        setClicked(true);
         setSorted(false);
-
+        setClicked(true);
+            console.log("rows[index]: " +rows[index]);
+            console.log("index: " +index);
             setCurrIndex(index);
-            setUpperIndex(index+1);
-            if (index >= length-1) {
+            setLowerIndex(index-1);
+            if (index > 0 && rows[index] < rows[index-1]) {
+                const temp = rows[index];
+                rows[index] = rows[index-1];
+                rows[index-1] = temp;
+            }
+            else {
                 setEnd(true);
             }
-            else if (Number(rows[index]) > Number(rows[index+1])) {
-                console.log("rows[index]: "+rows[index]);
-                console.log("rows[index+1]: "+rows[index+1]);
-                const temp = rows[index];
-                rows[index] = rows[index+1];
-                rows[index+1] = temp;
-                console.log(rows);
-            }
-
             setTimeout(() => {
                 setList(Object.assign([], rows));
               }, speed);
         
-
-        if (length <= 1) {
-            console.log('here');
-            setLength(items.length);
+        console.log("length: "+length);
+        console.log("end === " +end)
+        if (length >= rows.length) {
             setClicked(false);
             setSorted(true);
+            setLength(0);
+            setClicked(false);
             setCurrIndex(0);
-            setUpperIndex(currIndex);
+            setLowerIndex(currIndex-1);
             setIndex(0);
             setEnd(false);
             setSortClicked(false);
@@ -99,10 +96,10 @@ const BubbleSort = ({items, speed, setItems, sorting, setSorting}) => {
 
     useEffect(() => {
         setList(Object.assign([], items));
-        setLength(items.length);
+        setLength(0);
         setClicked(false);
         setCurrIndex(0);
-        setUpperIndex(currIndex);
+        setLowerIndex(currIndex-1);
         setIndex(0);
         setEnd(false);
         setSortClicked(false);
@@ -123,17 +120,19 @@ const BubbleSort = ({items, speed, setItems, sorting, setSorting}) => {
         <div className='list'>
             {list.map((item, i) => {
                 let color = 'white';
-
-                if (sorted) {
+                if (sorted === true) {
                     color = 'lightgreen';
                 }
-                else if (!end && (i === currIndex || i === upperIndex)) {
+                else if (i === currIndex || i === lowerIndex) {
                     color = 'red';
+                    if (end) {
+                        color = 'lightgreen';
+                    }
                 }
-                else if (end && i === currIndex) {
-                    color = 'lightgreen'
+                else if (end && currIndex === 0 && length > 0 && i === currIndex+1) {
+                    color = 'lightgreen';
                 }
-
+                
                 if (items.length <= 10) {
                     return (<div key={item} className="item" style={{height: `${(item*450)/items.length}px`, backgroundColor: `${color}`, fontSize: `2em`, fontWeight: `bold`, textAlign: `center`}}>
                         {item}
@@ -146,5 +145,4 @@ const BubbleSort = ({items, speed, setItems, sorting, setSorting}) => {
         </div>
     )
 }
-
-export default BubbleSort;
+export default InsertionSort;
